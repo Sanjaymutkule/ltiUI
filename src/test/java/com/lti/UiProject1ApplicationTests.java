1307f925345a37728702956140ff0232aa0ed580
Sanjaymutkule <sanjaymutkule8@gmail.com>
2021-09-06 18:57:40 +0530

    first commit

---
+++ src/test/java/com/lti/UiProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
